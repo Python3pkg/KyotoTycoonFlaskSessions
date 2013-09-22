892a392d1ca606ad6df3ee5a9fae60d00aeb7f3d
Quinlan Pfiffer <shithouse@goatse.cx>
2013-09-22 13:27:24 -0700

    Updated meta info.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 setup(
     author='Quinlan Pfiffer',
     author_email='qpfiffer@gmail.com',
+    url='https://github.com/qpfiffer/KyotoTycoonFlaskSessions',
     name='kt-flask-sessions',
     description='Kyoto Tycoon backed sessions for Flask',
     version='0.1.0',
